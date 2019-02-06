e32e4f2f3489b0577cafa07fe4cbd3af5ee12258
Joga <joga.gavidia@gmail.com>
2019-02-05 22:16:20 -0500

    Updates

---
+++ precache-manifest.0cddc610d217f02f30d7e063d92fdb7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b067752417da8740ecf9",
+    "url": "/cometoperu/static/js/runtime~main.b0677524.js"
+  },
+  {
+    "revision": "8a08cf3bd2870630812c",
+    "url": "/cometoperu/static/js/main.8a08cf3b.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/cometoperu/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "8a08cf3bd2870630812c",
+    "url": "/cometoperu/static/css/main.4be4da0a.chunk.css"
+  },
+  {
+    "revision": "bc4edbcdf22e97716dca5165fb81de40",
+    "url": "/cometoperu/index.html"
+  }
+];
